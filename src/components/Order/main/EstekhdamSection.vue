e22d4a1ad2a9d5ceca18edbc413014bfa19cc45e
kazhal1400 <quick.k1399@gmail.com>
2021-09-14 23:07:19 +0430

    write estekhdami section in order page

--- src/components/Order/main/EstekhdamSection.vue
+++ src/components/Order/main/EstekhdamSection.vue
@@ -1,160 +1,168 @@
 <template>
-  <div class=""></div>
-  <cllapse-btnone
-    :data_items="typeHamkari_items"
-    :marks="typeHamkariMarks"
-    @dataFn="typeHamkariFn"
-    id="typeHamkari"
-    exm="انتخاب"
-  />
+  <div class="mt-5">
+    <span class="fw-bold">شرح موقعیت شغلی</span>
+  </div>
 
+  <div class="mt-5">
+    <span class="fw-bold">نوع همکاری</span>
+    <select
+      class="form-select form-select-sm text-secondary py-2 pe-4"
+      aria-label=".form-select-sm example"
+      v-model="order_data.typeHamkari"
+    >
+      <option value="" selected disabled>انتخاب</option>
+      <option
+        class="text-start"
+        v-for="typeHamkari in typeHamkaris"
+        :key="typeHamkari"
+        :value="typeHamkari"
+      >
+        {{ typeHamkari }}
+      </option>
+    </select>
+  </div>
+  <div class="mt-5">
+    <label for="moaveze" class="text-muted px-3">امکان دورکاری وجود دارد</label>
+    <input
+      id="moaveze"
+      type="checkbox"
+      class="form-check-input"
+      value="true"
+      v-model="order_data.dorkari"
+    />
+  </div>
+  <div class="mt-5">
+    <span class="fw-bold">شیوهٔ پرداخت</span>
+    <select
+      class="form-select form-select-sm text-secondary py-2 pe-4"
+      aria-label=".form-select-sm example"
+      v-model="order_data.typePardakht"
+    >
+      <option value="" selected disabled>انتخاب</option>
+      <option
+        class="text-start"
+        v-for="typePardakht in typePardakhts"
+        :key="typePardakht"
+        :value="typePardakht"
+      >
+        {{ typePardakht }}
+      </option>
+    </select>
+  </div>
+  <div class="mt-5">
+    <span class="fw-bold">ساعات کاری</span>
+    <select
+      class="form-select form-select-sm text-secondary py-2 pe-4"
+      aria-label=".form-select-sm example"
+      v-model="order_data.timekar"
+    >
+      <option value="" selected disabled>انتخاب</option>
+      <option
+        class="text-start"
+        v-for="timekar in timekars"
+        :key="timekar"
+        :value="timekar"
+      >
+        {{ timekar }}
+      </option>
+    </select>
+  </div>
+  <div class="mt-5">
+    <div class="fw-bold mb-3">بیمه</div>
+    <label for="bimedarad" class="form-check-label mx-2">دارد</label>
+    <input
+      type="radio"
+      class="form-check-input ms-5"
+      id="bimedarad"
+      name="bime"
+      value="true"
+      @input="order_data.bime"
+    />
+    <label for="bimenadarad" class="form-check-label mx-2">ندارد</label>
+    <input
+      type="radio"
+      class="form-check-input ms-5"
+      id="bimenadarad"
+      name="bime"
+      value="false"
+      @input="order_data.bime"
+    />
+  </div>
   <hr />
-  <cllapse-btnone
-    :data_items="timekar_items"
-    :marks="timekarMarks"
-    @dataFn="timekarFn"
-    id="timekar"
-    exm="انتخاب"
-  />
+  <div class="mt-5">
+    <span class="fw-bold">شرایط و نیازمندی‌ها</span>
+    <p class="text-muted">
+      تکمیل این اطلاعات، کیفیت آگهی و شانس تعامل با کارجویانی متناسب با شرایط
+      شما را افزایش می‌دهد.
+    </p>
+  </div>
 
-  <hr />
-  <cllapse-btnone
-    :data_items="typePardakht_items"
-    :marks="typePardakhtMarks"
-    @dataFn="typePardakhtFn"
-    id="typePardakht"
-    exm="انتخاب"
-  />
+  <div class="mt-5">
+    <span class="fw-bold">سابقه</span>
+    <select
+      class="form-select form-select-sm text-secondary py-2 pe-4"
+      aria-label=".form-select-sm example"
+      v-model="order_data.sabeghe"
+    >
+      <option value="" selected disabled>انتخاب</option>
+      <option
+        class="text-start"
+        v-for="sabeghe in sabeghes"
+        :key="sabeghe"
+        :value="sabeghe"
+      >
+        {{ sabeghe }}
+      </option>
+    </select>
+  </div>
 
-  <hr />
-  <cllapse-btnone
-    :data_items="bime_items"
-    :marks="bimeMarks"
-    @dataFn="bimeFn"
-    id="bime"
-    exm="انتخاب"
-  />
-
-  <hr />
-  <cllapse-btnone
-    :data_items="dorkari_items"
-    :marks="dorkariMarks"
-    @dataFn="dorkariFn"
-    id="dorkari"
-    exm="انتخاب"
-  />
-
-  <hr />
-  <cllapse-btnone
-    :data_items="sabeghe_items"
-    :marks="sabegheMarks"
-    @dataFn="sabegheFn"
-    id="sabeghe"
-    exm="انتخاب"
-  />
-
-  <hr />
-  <cllapse-btnone
-    :data_items="sarbazi_items"
-    :marks="sarbaziMarks"
-    @dataFn="sarbaziFn"
-    id="sarbazi"
-    exm="انتخاب"
-  />
-
+  <div class="mt-5">
+    <span class="fw-bold">وضعیت سربازی</span>
+    <select
+      class="form-select form-select-sm text-secondary py-2 pe-4"
+      aria-label=".form-select-sm example"
+      v-model="order_data.sarbazi"
+    >
+      <option value="" selected disabled>انتخاب</option>
+      <option
+        class="text-start"
+        v-for="sarbazi in sarbazis"
+        :key="sarbazi"
+        :value="sarbazi"
+      >
+        {{ sarbazi }}
+      </option>
+    </select>
+  </div>
+   <hr />
+  <div class="mt-5">
+    <span class="fw-bold">اطلاعات آگهی</span>
+  </div>
 </template>
 
 <script>
-import { computed, ref } from "@vue/reactivity";
+import { computed } from "@vue/reactivity";
 import { inject } from "@vue/runtime-core";
-import CllapseBtnone from "../UI/CllapseBtnone.vue";
 
 export default {
-  components: {
-    CllapseBtnone,
-  },
+  components: {},
   setup() {
     const order_data = inject("order_data");
     const group_item_name = computed(() => order_data.group_item_name);
+    const typeHamkaris = computed(() => order_data.subItem.typeHamkari);
+    const typePardakhts = computed(() => order_data.subItem.typePardakht);
+    const timekars = computed(() => order_data.subItem.timekar);
+    const sabeghes = computed(() => order_data.subItem.sabeghe);
+    const sarbazis = computed(() => order_data.subItem.sarbazi);
 
-    const typeHamkariMarks = computed(() => order_data.subItem.typeHamkari);
-    const typeHamkari_items = ref("نوع همکاری");
-
-    const timekarMarks = computed(() => order_data.subItem.timekar);
-    const timekar_items = ref("ساعات کاری");
-
-    const typePardakhtMarks = computed(() => order_data.subItem.typePardakht);
-    const typePardakht_items = ref("شیوه پرداخت");
-
-    const bimeMarks = computed(() => order_data.subItem.bime);
-    const bime_items = ref("بیمه");
-
-    const dorkariMarks = computed(() => order_data.subItem.dorkari);
-    const dorkari_items = ref("امکان دورکاری");
-
-    const sabegheMarks = computed(() => order_data.subItem.sabeghe);
-    const sabeghe_items = ref("سابقه مورد نیاز");
-
-    const sarbaziMarks = computed(() => order_data.subItem.sarbazi);
-    const sarbazi_items = ref("پایان خدمت/معافیت");
-
-    function typeHamkariFn(typeHamkari) {
-      order_data.typeHamkari = typeHamkari;
-    }
-
-    function timekarFn(timekar) {
-      order_data.timekar = timekar;
-    }
-
-    function typePardakhtFn(typePardakht) {
-      order_data.typePardakht = typePardakht;
-    }
-
-    function bimeFn(bime) {
-      order_data.bime = bime;
-    }
-
-    function dorkariFn(dorkari) {
-      order_data.dorkari = dorkari;
-    }
-
-    function sabegheFn(sabeghe) {
-      order_data.sabeghe = sabeghe;
-    }
-
-    function sarbaziFn(sarbazi) {
-      order_data.sarbazi = sarbazi;
-    }
     return {
-      typeHamkariMarks,
-      typeHamkari_items,
-      typeHamkariFn,
-      
-      timekarMarks,
-      timekar_items,
-      timekarFn,
-      
-      typePardakhtMarks,
-      typePardakht_items,
-      typePardakhtFn,
-      
-      bimeMarks,
-      bime_items,
-      bimeFn,
-      
-      dorkariMarks,
-      dorkari_items,
-      dorkariFn,
-      
-      sabegheMarks,
-      sabeghe_items,
-      sabegheFn,
-
-      sarbaziMarks,
-      sarbazi_items,
-      sarbaziFn,
-
+      order_data,
       group_item_name,
+      typeHamkaris,
+      typePardakhts,
+      timekars,
+      sabeghes,
+      sarbazis,
     };
   },
 };
