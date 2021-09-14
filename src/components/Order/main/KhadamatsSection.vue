04b5decbb26a88183d56e967a2829b874343f3cb
kazhal1400 <quick.k1399@gmail.com>
2021-09-14 16:34:16 +0430

    write khadamat section in order page

--- src/components/Order/main/KhadamatsSection.vue
+++ src/components/Order/main/KhadamatsSection.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class=""></div>
+  <div class="mt-5">
+    <span class="fw-bold">لینک وب‌سایت</span>
+    <p class="texxt-muted">
+      لینک وب‌سایت شما، در وب‌سایت دیوار و در نسخهٔ ۱۰ اندروید قابل مشاهده و
+      کلیک است. هزینهٔ فعال‌سازی درج لینک در آگهی ۲۰,۰۰۰ تومان است.
+    </p>
+    <input
+      type="text"
+      class="input-group-text myinput"
+      placeholder="http://www.example.com"
+      v-model="order_data.khadamat_link"
+    />
+  </div>
 </template>
 
 <script>
-import { computed } from '@vue/reactivity';
-import { inject } from '@vue/runtime-core';
+import { computed } from "@vue/reactivity";
+import { inject } from "@vue/runtime-core";
 
 export default {
-  components: {
-    
-  },
+  components: {},
   setup() {
-     const order_data = inject("order_data");
+    const order_data = inject("order_data");
     const group_item_name = computed(() => order_data.group_item_name);
     return {
-      group_item_name
+      group_item_name,
+      order_data,
     };
   },
 };
